7406b154a3209b4f74480532319d48c2f4d1f6ab
Jianhui Gao <58570421+jianhuig@users.noreply.github.com>
2025-05-12 23:08:35 -0400

    Update SupervisedFairness.R

--- R/SupervisedFairness.R
+++ R/SupervisedFairness.R
@@ -6,7 +6,6 @@
 #' @param Y Outcome in the observed dataset.
 #' @param S Model score in observed dataset.
 #' @param A Group indicator in observed  dataset.
-#' @param W Weight. Default is Null.
 #' @param threshold Threshold for classification based on the model score.
 #' Default value is 0.5.
 #' @export
@@ -14,9 +13,8 @@
 SupervisedFairness <- function(Y,
                                S,
                                A,
-                               threshold = 0.5,
-                               W = NULL) {
-  est <- get_metric(Y, S, A, threshold, W = W)
+                               threshold = 0.5) {
+  est <- get_metric(Y, S, A, threshold)
   var <- Influence_curve(est, Y, S, A, threshold, method = "supervised")
 
   return(list(est = est, var = var))
