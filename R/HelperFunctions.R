abfb52598b35a993f9acd028a5337a3a5a87a812
Jianhui Gao <58570421+jianhuig@users.noreply.github.com>
2025-05-12 22:55:48 -0400

    Update HelperFunctions.R

--- R/HelperFunctions.R
+++ R/HelperFunctions.R
@@ -46,131 +46,49 @@ FitParametricCalibration <- function(Y_labeled,
 }
 
 # Compute all the metrics
-
-get_metric <- function(Y, S, A, threshold = 0.5, W = NULL) {
-  if (is.null(W)) {
-    W <- rep(1, length(Y))
-  }
+get_metric <- function(Y, S, A, threshold = 0.5) {
   class <- sort(unique(A))
   out <- c()
+  D <- 1 * (S > threshold)
   for (i in class) {
-    C <- 1 * (S[A == i] > threshold)
-    P <- sum(Y[A == i] * W[A == i])
-    N <- sum((1 - Y[A == i]) * W[A == i])
-    TP <- sum(Y[A == i] * C * W[A == i])
-    FP <- sum((1 - Y[A == i]) * C * W[A == i])
-    TN <- N - FP
-    FN <- P - TP
-    PP <- TP + FP
-    PN <- FN + TN
+    mu_Y <- mean(Y[A == i])
+    mu_D <- mean(D[A == i])
+    mu_DY <- mean(D[A == i] * Y[A == i])
+    mu_SY <- mean(S[A == i] * Y[A == i])
+    mu_S2 <- mean(S[A == i]**2)
+    
+    print(paste0("mu_Y: ", mu_Y))
+    print(paste0("mu_D: ", mu_D))
+    print(paste0("mu_DY: ", mu_DY))
+    tpr <- mu_DY / mu_Y
+    print(paste0("tpr: ", tpr))
+    fpr <- (mu_D - mu_DY) / (1 - mu_Y)
+    ppv <- mu_DY / mu_D
+    npv <- (1 - mu_D - mu_Y + mu_DY) / (1 - mu_D)
+    f1 <- 2 * mu_DY / (mu_D + mu_Y)
+    acc <- 1 - mu_Y - mu_D + 2 * mu_DY
+    bs <- mu_S2 - 2 * mu_SY + mu_Y
 
-    tpr <- TP / P
-    tnr <- TN / (TN + FP)
-    fpr <- FP / N
-    fnr <- FN / (TP + FN)
-    npv <- TN / PN
-    ppv <- TP / PP
-    acc <- (TP + TN) / (P + N)
-    f1 <- (2 * TP) / (2 * TP + FP + FN)
-    bs <- mean(Y[A == i] * W[A == i] +
-      (S[A == i])**2 * W[A == i] -
-      2 * Y[A == i] * S[A == i] * W[A == i]) / mean(W[A == i])
-    out <- c(out, tpr, tnr, fpr, fnr, npv, ppv, acc, f1, bs)
+    out <- c(out, tpr, fpr, ppv, npv, f1, acc, bs)
+    print(out)
+    
   }
   if (length(class) == 2) {
     out <- cbind(matrix(out, ncol = 2, byrow = FALSE), NA)
     out[, 3] <- out[, 1] - out[, 2]
     colnames(out) <- c(paste0("Group", class), "Delta")
   } else {
-    out <- matrix(out, ncol = length(class), byrow = FALSE)
-    mad <- as.numeric(apply(out, 1, mad))
-    var <- as.numeric(apply(out, 1, var))
-    gei <- as.numeric(apply(out, 1, entropy))
-    out <- cbind(out, mad, var, gei)
-    colnames(out) <- c(paste0("Group", class), "mad", "var", "gei")
+    return(error("More than 2 groups not supported"))
   }
-  rownames(out) <- c("TPR", "TNR", "FPR", "FNR", "NPV", "PPV", "ACC", "F1", "BS")
+  rownames(out) <- c("TPR", "FPR", "PPV", "NPV", "F1", "ACC", "BS")
   tibble::rownames_to_column(as.data.frame(out), "Metric")
 }
+
+
 
 # Indicator function in R
 Indicator <- function(x) {
   ifelse(I(x), 1, 0)
-}
-
-
-# computes the AUC
-AUC.FUN <- function(data) {
-  dd <- data[, 1]
-  xx <- data[, 2]
-  n0 <- sum(1 - dd)
-  n1 <- sum(dd)
-  x0 <- xx[dd == 0]
-  x1 <- xx[dd == 1]
-  sum((sum.I(x0, "<=", x1) + sum.I(x0, "<", x1)) / 2) / (n0 * n1)
-}
-
-
-# Computes sums efficiently based on ranks
-sum.I <- function(yy, FUN, Yi, Vi = NULL, ties.method = "first") {
-  if (FUN == "<" | FUN == ">=") {
-    yy <- -yy
-    Yi <- -Yi
-  }
-  pos <- rank(c(yy, Yi), ties.method = ties.method)[1:length(yy)] -
-    rank(yy, ties.method = ties.method)
-  if (substring(FUN, 2, 2) == "=") pos <- length(Yi) - pos
-  if (!is.null(Vi)) {
-    if (substring(FUN, 2, 2) == "=") tmpind <- order(-Yi) else tmpind <- order(Yi)
-    Vi <- apply(as.matrix(Vi)[tmpind, , drop = F], 2, cumsum)
-    return(rbind(0, Vi)[pos + 1, ])
-  } else {
-    return(pos)
-  }
-}
-
-# Computes the natural spline basis
-ns.basis <- function(X, nk) {
-  ## X: covariate matrix
-  ## nk: number of knots
-
-  X <- as.matrix(X)
-  basis <- NULL
-
-  for (i in 1:ncol(X)) {
-    X_i <- X[, i]
-
-    # quantiles to determine appropriate knots
-    knots <- quantile(X_i, seq(0, 1, length = nk))
-
-    # changes quantiles if there aren't enough unique values
-    j <- 0
-    while (length(unique(knots)) != nk) {
-      j <- j + 1
-      knots <- unique(quantile(X_i, seq(0, 1, length = nk + j)))
-    }
-
-    # compute the natural spline basis
-    d_k <- (trunc.cub(X_i, knots[nk - 1])) / (knots[nk] - knots[nk - 1])
-    evals <- sapply(1:(nk - 2), function(ii) {
-      d_i <- (trunc.cub(X_i, knots[ii])) / (knots[nk] - knots[ii])
-      basis.new <- d_i - d_k
-    })
-
-    # bind together and include original variable
-    basis <- cbind(basis, cbind(X_i, evals))
-
-    # cbind(X_i, evals))
-  }
-  return(basis)
-}
-
-# Computes the truncated cubic
-trunc.cub <- function(X, x) {
-  ## X: variable of interest
-  ## x: knot location
-
-  ((X > x) * (X - x))^3
 }
 
 expit <- function(x) {
